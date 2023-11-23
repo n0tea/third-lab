f60270771d90d8c9d4fefcd1f8e57153b9d4d262
n0tea <artnotea04@gmail.com>
2023-11-23 18:36:57 +0300

    Update main proj.cs

--- main proj.cs
+++ main proj.cs
@@ -3,6 +3,7 @@ internal class Program
     static void Main(string[] args)
     {
         int dev = 100;
-        Console.WriteLine($"{dev-10}");
+        Console.WriteLine($"{dev-20}");
+        Console.WriteLine($"hype");
     }
 }
